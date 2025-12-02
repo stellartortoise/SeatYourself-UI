6478dc76b0269b99a42fc6a0abfc38953d8e1067
apb-nscc <86730691+stellartortoise@users.noreply.github.com>
2025-12-02 12:35:21 -0400

    Got Posting to Work for Comments

--- src/routes/Comment.jsx
+++ src/routes/Comment.jsx
@@ -1,22 +1,49 @@
 import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { Link} from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 function Comment() {
   const { id } = useParams();
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = async(data) => {
+    console.log(data);
+
+    const response = await fetch(import.meta.env.VITE_API_URL + '/comments', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+            body: JSON.stringify(data)
+        });
+
+        const result = await response.json();
+        console.log(result);
+
+        // if (response.ok) {
+        //     alert('Comment submitted successfully!');
+        // } else {
+        //     alert('Failed to submit comment.');
+        // }
+    }
 
     return (
         <>
             <p><Link to={`/details/${id}`}>← Back to Occasion</Link></p>
             
-            <form className="w-100 p-3 border rounded">
+            <form onSubmit={handleSubmit(onSubmit)} className="w-100 p-3 border rounded">
+                <input type="hidden" value={id} {...register('OccasionId')} />
+                {/* <input type="hidden" value={new Date().toISOString()} {...register('CreatedAt')} /> */}
+
                 <div className="mb-3">
-                    <label for="author" className="form-label">Author</label>
-                    <input type="text" className="form-control" id="author" />
+                    <label htmlFor="Author" className="form-label">Author</label>
+                    <input type="text" className="form-control" id="Author"
+                     {...register('Author')} />
                 </div>
                 <div className="mb-3">
-                    <label for="body" className="form-label">Body</label>
-                    <textarea className="form-control" id="body" rows="3"></textarea>
-                    {/* <input type="" className="form-control" id="body" /> */}
+                    <label htmlFor="Body" className="form-label">Body</label>
+                    <textarea className="form-control" id="Body" rows="3" 
+                    {...register('Body')}></textarea>
                 </div>
 
                 <button type="submit" className="btn btn-primary">Submit</button>
